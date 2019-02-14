ab07a58a4b939cc3c24b63c87c36e0edede69617
ciripel <nedelcu.alexandru@yahoo.com>
2019-02-15 00:53:09 +0200

    setting up

--- src/config.js
+++ src/config.js
@@ -4,21 +4,21 @@ config.js - Configuration for ZENCash Coin
 
 module.exports = {
   mainnet: {
-    messagePrefix: 'ZENCash main net',
+    messagePrefix: 'Snowgem main net',
     bip32: {
       public: '0488b21e',
       private: '0488ade4'
     },
-    pubKeyHash: '2089',
-    scriptHash: '2096',
+    pubKeyHash: '1c28',
+    scriptHash: '1c2d',
     zcPaymentAddressHash: '169a', // Private z-address
     zcSpendingKeyHash: 'ab36', // Spending key
     wif: '80'
   },
   testnet: {
     wif: 'ef',
-    pubKeyHash: '2098',
-    scriptHash: '2092',
+    pubKeyHash: '1d25',
+    scriptHash: '1cba',
     zcPaymentAddressHash: '16b6',
     zcSpendingKeyHash: 'ac08'
   }
